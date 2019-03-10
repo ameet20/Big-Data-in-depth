744b702b4cb83122633119059dda339e7d3b1920
Ameeta Dhas <dhas.ameet@gmail.com>
2019-03-10 16:13:19 +0530

    UsesSetState.java

---
+++ Beam/sdks/java/src/testing/UsesSetState.java
@@ -0,0 +1,6 @@
+package org.apache.beam.sdk.testing;
+
+import org.apache.beam.sdk.state.SetState;
+
+/** Category tag for validation tests which utilize {@link SetState}. */
+public interface UsesSetState {}
