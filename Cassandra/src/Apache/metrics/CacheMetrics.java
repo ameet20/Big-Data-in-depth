f362503c19099d2e71c4f4c0f1720ecb445c46de
Ameeta Dhas <dhas.ameet@gmail.com>
2019-03-12 14:51:36 +0530

    CacheMetrics.java

---
+++ Cassandra/src/Apache/metrics/CacheMetrics.java
@@ -0,0 +1,119 @@
+package org.apache.cassandra.metrics;
+
+import java.util.function.DoubleSupplier;
+
+import com.codahale.metrics.*;
+import org.apache.cassandra.cache.CacheSize;
+
+import static org.apache.cassandra.metrics.CassandraMetricsRegistry.Metrics;
+
+/**
+ * Metrics for {@code ICache}.
+ */
+public class CacheMetrics
+{
+    /** Cache capacity in bytes */
+    public final Gauge<Long> capacity;
+    /** Total size of cache, in bytes */
+    public final Gauge<Long> size;
+    /** Total number of cache entries */
+    public final Gauge<Integer> entries;
+
+    /** Total number of cache hits */
+    public final Meter hits;
+    /** Total number of cache misses */
+    public final Meter misses;
+    /** Total number of cache requests */
+    public final Metered requests;
+
+    /** all time cache hit rate */
+    public final Gauge<Double> hitRate;
+    /** 1m hit rate */
+    public final Gauge<Double> oneMinuteHitRate;
+    /** 5m hit rate */
+    public final Gauge<Double> fiveMinuteHitRate;
+    /** 15m hit rate */
+    public final Gauge<Double> fifteenMinuteHitRate;
+
+    protected final MetricNameFactory factory;
+
+    /**
+     * Create metrics for given cache.
+     *
+     * @param type Type of Cache to identify metrics.
+     * @param cache Cache to measure metrics
+     */
+    public CacheMetrics(String type, CacheSize cache)
+    {
+        factory = new DefaultNameFactory("Cache", type);
+
+        capacity = Metrics.register(factory.createMetricName("Capacity"), cache::capacity);
+        size = Metrics.register(factory.createMetricName("Size"), cache::weightedSize);
+        entries = Metrics.register(factory.createMetricName("Entries"), cache::size);
+
+        hits = Metrics.meter(factory.createMetricName("Hits"));
+        misses = Metrics.meter(factory.createMetricName("Misses"));
+        requests = Metrics.register(factory.createMetricName("Requests"), sumMeters(hits, misses));
+
+        hitRate =
+            Metrics.register(factory.createMetricName("HitRate"),
+                             ratioGauge(hits::getCount, requests::getCount));
+        oneMinuteHitRate =
+            Metrics.register(factory.createMetricName("OneMinuteHitRate"),
+                             ratioGauge(hits::getOneMinuteRate, requests::getOneMinuteRate));
+        fiveMinuteHitRate =
+            Metrics.register(factory.createMetricName("FiveMinuteHitRate"),
+                             ratioGauge(hits::getFiveMinuteRate, requests::getFiveMinuteRate));
+        fifteenMinuteHitRate =
+            Metrics.register(factory.createMetricName("FifteenMinuteHitRate"),
+                             ratioGauge(hits::getFifteenMinuteRate, requests::getFifteenMinuteRate));
+    }
+
+    private static Metered sumMeters(Metered first, Metered second)
+    {
+        return new Metered()
+        {
+            @Override
+            public long getCount()
+            {
+                return first.getCount() + second.getCount();
+            }
+
+            @Override
+            public double getMeanRate()
+            {
+                return first.getMeanRate() + second.getMeanRate();
+            }
+
+            @Override
+            public double getOneMinuteRate()
+            {
+                return first.getOneMinuteRate() + second.getOneMinuteRate();
+            }
+
+            @Override
+            public double getFiveMinuteRate()
+            {
+                return first.getFiveMinuteRate() + second.getFiveMinuteRate();
+            }
+
+            @Override
+            public double getFifteenMinuteRate()
+            {
+                return first.getFifteenMinuteRate() + second.getFifteenMinuteRate();
+            }
+        };
+    }
+
+    private static RatioGauge ratioGauge(DoubleSupplier numeratorSupplier, DoubleSupplier denominatorSupplier)
+    {
+        return new RatioGauge()
+        {
+            @Override
+            public Ratio getRatio()
+            {
+                return Ratio.of(numeratorSupplier.getAsDouble(), denominatorSupplier.getAsDouble());
+            }
+        };
+    }
+}
