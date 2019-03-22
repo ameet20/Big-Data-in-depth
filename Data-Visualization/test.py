578b933f21e2b55d9907c6a43bf6927bdfd84661
Ameet Dhas <dhas.ameet@gmail.com>
2019-03-22 15:19:12 +0530

    test.py

---
+++ Data-Visualization/test.py
@@ -0,0 +1,20 @@
+import sys
+import pytest
+
+args = sys.argv[1:]
+if '--pyside' in args:
+    args.remove('--pyside')
+    import PySide
+elif '--pyqt4' in args:
+    args.remove('--pyqt4')
+    import PyQt4
+elif '--pyqt5' in args:
+    args.remove('--pyqt5')
+    import PyQt5
+
+import pyqtgraph as pg
+pg.systemInfo()
+
+pytest.main(args)
+    
+    
