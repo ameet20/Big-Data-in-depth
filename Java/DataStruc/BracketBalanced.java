d8553293aa1eecaaef53697c1deb6a36678c659a
Ameet Dhas <dhas.ameet@gmail.com>
2019-05-06 11:47:41 +0530

    BracketBalanced.java

---
+++ Java/DataStruc/BracketBalanced.java
@@ -0,0 +1,68 @@
+package data_structures.Stacks;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+class BalancedBrackets {
+
+static boolean is_balanced(String s) {
+        Stack<Character> bracketsStack = new Stack<>();
+        char[] text = s.toCharArray();
+        for (char x : text) {
+            switch (x) {
+                case '{':
+                case '<':
+                case '(':
+                case '[':
+                    bracketsStack.push(x);
+                    break;
+                case '}':
+                    if (bracketsStack.peek() == '{') {
+                        bracketsStack.pop();
+                        break;
+                    } else {
+                        return false;
+                    }
+                case '>':
+                    if (bracketsStack.peek() == '<') {
+                        bracketsStack.pop();
+                        break;
+                    } else {
+                        return false;
+                    }
+                case ')':
+                    if (bracketsStack.peek() == '(') {
+                        bracketsStack.pop();
+                        break;
+                    } else {
+                        return false;
+                    }
+                case ']':
+                    if (bracketsStack.peek() == '[') {
+                        bracketsStack.pop();
+                        break;
+                    } else {
+                        return false;
+                    }
+            }
+        }
+        return bracketsStack.empty();
+    }
+
+    /**
+     *
+     * @param args
+     * @TODO remove main method and Test using JUnit or other methodology
+     */
+    public static void main(String args[]) {
+        try (Scanner in = new Scanner(System.in)) {
+            System.out.println("Enter sequence of brackets: ");
+            String s = in.nextLine();
+            if (is_balanced(s)) {
+                System.out.println(s + " is balanced");
+            } else {
+                System.out.println(s + " ain't balanced");
+            }
+        }
+    }
+}
