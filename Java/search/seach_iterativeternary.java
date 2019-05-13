cdde9bf3f6285f08855f50fde4a07ddd9e31c494
Ameet Dhas <dhas.ameet@gmail.com>
2019-05-13 12:28:17 +0530

    seach_iterativeternary.java

---
+++ Java/search/seach_iterativeternary.java
@@ -0,0 +1,66 @@
+package Searches;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.stream.Stream;
+
+import static java.lang.String.format;
+
+public class IterativeTernarySearch implements SearchAlgorithm {
+
+
+    @Override
+    public <T extends Comparable<T>> int find(T[] array, T key) {
+        int left = 0;
+        int right = array.length - 1;
+
+        while (right > left) {
+
+            int leftCmp = array[left].compareTo(key);
+            int rightCmp = array[right].compareTo(key);
+            if (leftCmp == 0) return left;
+            if (rightCmp == 0) return right;
+
+            int leftThird = left + (right - left) / 3 + 1;
+            int rightThird = right - (right - left) / 3 - 1;
+
+
+            if (array[leftThird].compareTo(key) <= 0) {
+                left = leftThird;
+            } else {
+                right = rightThird;
+            }
+        }
+
+        return -1;
+    }
+
+
+    public static void main(String[] args) {
+        //just generate data
+        Random r = new Random();
+        int size = 100;
+        int maxElement = 100000;
+        Integer[] integers = Stream.generate(() -> r.nextInt(maxElement))
+                .limit(size)
+                .sorted()
+                .toArray(Integer[]::new);
+
+
+        //the element that should be found
+        Integer shouldBeFound = integers[r.nextInt(size - 1)];
+
+        IterativeTernarySearch search = new IterativeTernarySearch();
+        int atIndex = search.find(integers, shouldBeFound);
+
+        System.out.println(format("Should be found: %d. Found %d at index %d. An array length %d",
+                shouldBeFound, integers[atIndex], atIndex, size));
+
+        int toCheck = Arrays.binarySearch(integers, shouldBeFound);
+        System.out.println(format("Found by system method at an index: %d. Is equal: %b",
+                toCheck, toCheck == atIndex));
+
+    }
+
+
+}
